fac2b5755518c0ffe21512d0f402db6027efe365
syi07030 <syii07030@gmail.com>
2022-03-21 22:22:47 +0900

    feat: Search Bar

--- Haru3cut/PageView/TabView.swift
+++ Haru3cut/PageView/TabView.swift
@@ -10,15 +10,44 @@ import UIKit
 
 struct MyTabView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @State private var showSearch = false
+    @State private var isEditing = false
+    @State var searchText = ""
      
      var body: some View {
+         ZStack{
          VStack {
-            topView()
+             HStack(spacing:20){
+                     Image("profile")
+                     .frame(width: 70, height: 70, alignment: .center)
+                     .cornerRadius(50)
+                     .overlay(RoundedRectangle(cornerRadius: 50)
+                                 .stroke(Color.black, lineWidth: 2))
+                     Text("\(name)")
+                     .font(.title)
+                     Spacer()
+                     Image(systemName: "pencil")
+                     .resizable()
+                     .frame(width:25, height:25)
+                     .padding()
+                 
+                 Button(action:{showSearch.toggle()}){
+                     Image(systemName: "magnifyingglass")
+                     .resizable()
+                     .frame(width:25, height:25)
+                 }
+                     
+                     Image("dots")
+                     .resizable()
+                     .frame(width:40, height:40)
+                         
+             }
                  .padding()
                  .padding(.top, 70)
                  .frame(height:100)
                  
-            Divider().frame(width:500)
+            Divider()
                  .padding(.top,30)
                               
              TabView{
@@ -42,13 +71,21 @@ struct MyTabView: View {
              }
              .accentColor(Color.pp)
              
+         }
+             
+             if showSearch{
+                 SearchBar(text: $searchText)
              }
-             .ignoresSafeArea(.all)
-                .navigationBarBackButtonHidden(true)
+            
+         }.ignoresSafeArea(.all)
+             .navigationBarBackButtonHidden(true)
      }
 }
 
 struct topView: View{
+    @State private var showSearch = false
+    @State private var isEditing = false
+    @State var searchText = ""
     var body: some View{
         HStack(spacing:20){
                 Image("profile")
@@ -63,9 +100,13 @@ struct topView: View{
                 .resizable()
                 .frame(width:25, height:25)
                 .padding()
+            
+            Button(action:{showSearch.toggle()}){
                 Image(systemName: "magnifyingglass")
                 .resizable()
                 .frame(width:25, height:25)
+            }
+                
                 Image("dots")
                 .resizable()
                 .frame(width:40, height:40)
@@ -76,6 +117,48 @@ struct topView: View{
                        .frame(width: 50, height: 50, alignment: .leading)
                  */
                     
+        }
+        
+        ZStack{
+            if showSearch{
+                SearchBar(text: $searchText)
+            }
+        }
+    }
+}
+
+struct SearchBar: View {
+    @Binding var text: String
+ 
+    @State private var isEditing = false
+ 
+    var body: some View {
+        VStack{
+        ZStack {
+ 
+            TextField("Search ...", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+                .padding(.top,130)
+ 
+            if isEditing {
+                Button(action: {
+                    self.text = ""
+ 
+                }) {
+                    Image(systemName: "x.circle")
+                }
+                //.padding(.trailing, 10)
+                //.transition(.move(edge: .trailing))
+            }
+        }
+            Spacer()
         }
     }
 }
