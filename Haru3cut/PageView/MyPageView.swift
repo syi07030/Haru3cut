c9c8753a400f01531c6e871e36ea46980701fd26
syi07030 <syii07030@gmail.com>
2022-03-23 23:54:12 +0900

    feat: MyPage

--- Haru3cut/PageView/MyPageView.swift
+++ Haru3cut/PageView/MyPageView.swift
@@ -11,13 +11,39 @@ struct MyPageView: View {
  
     var body: some View {
         VStack{
-            Spacer()
-            Text("My page")
-            Spacer()
-            Divider()
+            HStack(spacing:20){
+                Image("profile")
+                    .frame(width: 70, height: 70, alignment: .center)
+                    .cornerRadius(50)
+                    .overlay(RoundedRectangle(cornerRadius: 50)
+                    .stroke(Color.black, lineWidth: 2))
+                Text("\(name)")
+                    .font(.title)
+                Spacer()
+                Button(action:{}){
+                    Text("프로필 수정")
+                        .padding()
+                        .font(.caption)
+                        .background(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2))
+                }.accentColor(Color.pp)
+            }
+            .padding()
+            .frame(height:80)
+            
+            List{
+                Section(header: Text("header"), footer: Text("footer"), content: { Button("친구 관리",action: {})
+                    Button("모임 관리",action: {})
+                    Button("목표 설정",action: {})
+                    Button("자주 쓰는 태그 관리",action: {})
+                    Button("일반 설정",action: {})
+                    Button("앱/웹/프로젝트 정보",action: {})
+                    
+                }).listStyle(DefaultListStyle())
+
+            }
         }
-        }
-   }
+    }
+}
 
 #if DEBUG
 struct MyPageView_Previews: PreviewProvider {
