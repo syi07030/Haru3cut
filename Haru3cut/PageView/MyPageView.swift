d6861647e3f176fee7cca3fb628d594885fa0597
syi07030 <syii07030@gmail.com>
2022-04-04 18:26:47 +0900

    fix: list style

--- Haru3cut/PageView/MyPageView.swift
+++ Haru3cut/PageView/MyPageView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MyPageView: View {
  
     var body: some View {
+
         NavigationView{
         VStack{
             HStack(spacing:20){
@@ -24,60 +25,68 @@ struct MyPageView: View {
                 Button(action:{}){
                     Text("프로필 수정")
                         .padding()
-                        //.font(.caption)
-                        //.background(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2))
                         .border(Color.pp)
                 }.accentColor(Color.pp)
             }
             .padding()
-            .frame(height:80)
+            .frame(height:85)
             
             List{
-                /*
-                Section(content: {
-                    Button("친구 관리",action: {})
-                    Button("모임 관리",action: {})
-                    Button("목표 설정",action: {})
-                    Button("자주 쓰는 태그 관리",action: {})
-                    Button("일반 설정",action: {})
-                    Button("앱/웹/프로젝트 정보",action: {})
-                })
-                 */
-                
-                Section(header: Text("친구 관리"), content: {
+                Section(header: Text("친구 관리").padding(.top,20).padding(.bottom,10).foregroundColor(Color.pp), content: {
                     NavigationLink(destination: DetailPageView(message: "친구 추가"), label: {Text("친구 추가")})
                     Button("친구 삭제",action: {})
                     Button("친구 차단",action: {})
                 })
-                Section(header: Text("모임 관리"), content: {
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
+                //.padding(.top,10)
+                Section(header: Text("모임 관리").padding(.bottom,10).foregroundColor(Color.pp), content: {
                     Button("모임 가입",action: {})
                     Button("모임 탈퇴",action: {})
                     Button("모임 생성",action: {})
                     Button("모임 검색",action: {})
                 })
-                Section(header: Text("목표 설정"), content: {
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
+                Section(header: Text("목표 설정").padding(.bottom,10).foregroundColor(Color.pp), content: {
                     Button("목표 추가",action: {})
                     Button("목표 수정",action: {})
                 })
-                Section(header: Text("태그 관리"), content: {
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
+                Section(header: Text("태그 관리").padding(.bottom,10).foregroundColor(Color.pp), content: {
                     Button("태그 추가",action: {})
                     Button("태그 삭제",action: {})
                     Button("태그 순서 변경",action: {})
                 })
-                Section(header: Text("일반 설정"), content: {
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
+                Section(header: Text("일반 설정").padding(.bottom,10).foregroundColor(Color.pp), content: {
                     Button("화면 모드 설정",action: {})
                     Button("알림 설정",action: {})
                     Button("언어 설정",action: {})
                     Button("로그아웃",action: {})
                     Button("회원탈퇴",action: {})
                 })
-                Section(header: Text("정보"), content: {
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
+                Section(header: Text("정보").padding(.bottom,10).foregroundColor(Color.pp), content: {
                     Button("앱 정보",action: {})
                     Button("웹 정보",action: {})
                     Button("프로젝트 정보",action: {})
                 })
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(Color(UIColor.systemGray6))
             }
-            .listStyle(InsetGroupedListStyle())
+            .listStyle(SidebarListStyle())
+            
+            //.listStyle(Carouse)
             
         }
         .hiddenNavigationBarStyle()
@@ -111,7 +120,7 @@ extension View {
 #if DEBUG
 struct MyPageView_Previews: PreviewProvider {
     static var previews: some View {
-        PartyPageView()
+        MyPageView()
     }
 }
 #endif
