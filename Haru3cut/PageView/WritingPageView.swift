c2896a54c540a152eda69566075b5e7ada140509
syi07030 <syii07030@gmail.com>
2022-04-07 21:01:44 +0900

    fix: Writing Tag issue -ing

--- Haru3cut/PageView/WritingPageView.swift
+++ Haru3cut/PageView/WritingPageView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftUI
+import TagTextField
 
 struct WritingPageView: View {
     @Environment(\.presentationMode) var presentationMode
@@ -19,6 +20,62 @@ struct WritingPageView: View {
     
     @State var tag = ""
     @State var tagClick = false
+    
+    @State var tags = [String]()
+    @State var keyword = ""
+    
+    var allTags = ["운동", "일상", "취미"]
+    
+    var theme: TagTheme {
+           return TagTheme(font: Font.system(size: 15), foregroundColor: Color.white, backgroundColor: Color.black, borderColor: Color.white, shadowColor: Color.green, deletable: false, deleteButtonSize: 10, deleteButtonColor: Color.yellow, spacing: 5, alignment: .leading, inputFieldTextColor: Color.black)
+       }
+    
+    @ViewBuilder
+        var searchView: some View {
+            HStack(alignment: .top, spacing: 10) {
+                Text("To")
+                    .padding(.top, 4)
+                TagTextField(tags: $tags, keyword: $keyword, theme: self.theme, placeholder: "Tags") { _ in
+                    return allTags.filter{$0.lowercased().contains(keyword.lowercased()) == true}.first
+                }
+                .padding(6)
+                .background(Color(.sRGB, red: 224.0/255.0, green: 224.0/255.0, blue: 225.0/255.0, opacity: 1))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+            }
+            .padding(16)
+        }
+        
+        @ViewBuilder
+        var searchResultView: some View {
+            if (allTags.filter{$0.lowercased().contains(keyword.lowercased()) == true}).count > 0 {
+                List {
+                    ForEach((allTags.filter{$0.lowercased().contains(keyword.lowercased()) == true}), id: \.self) { tag in
+                        Button(action: {
+                            if tags.contains(tag) == false {
+                                tags.append(tag)
+                            }
+                            keyword = ""
+                        }, label: {
+                            Text(tag)
+                                .padding()
+                        })
+                        .listRowInsets(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+                    }
+                }
+            }
+            else {
+                VStack(alignment: .center, spacing: 0) {
+                    Spacer()
+                    
+                    Text("No tags found")
+                        .font(Font.system(size: 16))
+                        .foregroundColor(Color.secondary)
+                        .padding(.top, 20)
+                    
+                    Spacer()
+                }
+            }
+        }
     
     var body: some View {
         VStack{
@@ -86,17 +143,20 @@ struct WritingPageView: View {
                     .background(Color.white)
                     .padding()
             }
-            TextField("Enter the tag",text: $tag,onEditingChanged: { edit in self.tagClick = edit })
-                .padding()
-                .textFieldStyle(TextStyle(focused: $tagClick))
+            
+            searchView
+            searchResultView
+            
             ScrollView{
                 ForEach(0..<20, id:\.self){ num in
                     HStack{
                         Text("\(num)").padding()
                         Spacer()
-                    }.padding(.leading,15)
+                    }
                 }
-            }
+            } .background(Color.ww)
+                .padding(.leading)
+                .padding(.trailing)
             Spacer()
             
         }//.navigationBarTitleDisplayMode(.inline)
@@ -151,8 +211,8 @@ class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerContro
         self.picker = picker
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let selectedImage = info[.originalImage] as? UIImage else {return}
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
         self.picker.selectedImage = selectedImage
         self.picker.isPresented.wrappedValue.dismiss()
     }
